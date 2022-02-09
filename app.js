144684c10a79c0ae5193ea971e5578b8b39d713d
KennethKosling <kenkoz7164@gmail.com>
2022-02-08 20:24:41 -0500

    finished review activites

---
+++ app.js
@@ -0,0 +1,81 @@
+//Activity 1-------------------------------------------------------------
+
+let a = '';
+let b = 'bongos'
+let c = 'get your'
+
+/*
+a = b
+b = c
+c = a
+*/
+
+/* Solution: 
+a = 'bongos'
+b = 'get your'
+c = 'bongos'
+*/
+
+//Activity 2-------------------------------------------------------------
+
+/*
+console.log('Please' + ' squeeze' + ' the ' + 'cheese')
+
+const word = 'believe'
+console.log('Please ' + word + ' the ' + 'cheese')
+
+const five = 5
+const ten = 10
+const sum = five + ten
+console.log('The sum of ' + five + ' and ' + ten + ' is ' + sum)
+*/
+
+//Activity 3-------------------------------------------------------------
+
+/*
+console.log(-10 >= -100)
+console.log(0 < 1000)
+console.log(888 == 889)
+console.log(20 <= 20)
+*/
+
+//Activity 4-------------------------------------------------------------
+
+/*
+let num = 1
+
+while (num <= 100){
+    console.log(num);
+    num++;
+}
+
+let i = 0
+
+while (i <= 5000){
+    console.log(i * i);
+    i++;
+}
+
+let i = 1000
+
+while(i >= 1){
+    console.log(i);
+    i--;
+}
+*/
+
+//Activity 5-------------------------------------------------------------
+
+/*
+for (let i = 999; i <= 9999; i++){
+    console.log(i);
+}
+
+for (let i = 1; i <= 100; i++){
+    console.log("This is the song that never ends");
+}
+
+for (let i = 1000; i >= 1; i--){
+    console.log(i);
+}
+*/
